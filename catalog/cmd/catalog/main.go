d672628f7c644fc514a6a2ac981942e29f718fbe
pradeep <pradytpk@gmail.com>
2024-11-16 00:08:04 +0900

    catalog main.go code

--- catalog/cmd/catalog/main.go
+++ catalog/cmd/catalog/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/pradytpk/go-ms-grpc/catalog"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var r catalog.Repository
+	retry.ForeverSleep(2*time.Second, func(i int) error {
+		fmt.Println("Database URL:", cfg.DatabaseURL)
+		r, err = catalog.NewElasticRepository(cfg.DatabaseURL)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		return nil
+	})
+	defer r.Close()
+	log.Println("listening on port 8080")
+	s := catalog.NewService(r)
+	log.Fatal(catalog.ListenGRPC(s, 8080))
+}
