38508a51c58937abefbbdb8cc88f2619f6a81f98
pradeep <pradytpk@gmail.com>
2024-11-18 20:48:50 +0900

    order service code

--- order/service.go
+++ order/service.go
@@ -3,6 +3,8 @@ package order
 import (
 	"context"
 	"time"
+
+	"github.com/segmentio/ksuid"
 )
 
 type Order struct {
@@ -32,12 +34,26 @@ type orderService struct {
 
 // GetOrdersForAccount implements Service.
 func (o *orderService) GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error) {
-	panic("unimplemented")
+	return o.repository.GetOrdersForAccount(ctx, accountID)
 }
 
 // PostOrder implements Service.
 func (o *orderService) PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error) {
-	panic("unimplemented")
+	or := &Order{
+		ID:        ksuid.New().String(),
+		CreatedAt: time.Now().UTC(),
+		AccountID: accountID,
+		Products:  products,
+	}
+	or.TotalPrice = 0.0
+	for _, v := range products {
+		or.TotalPrice += v.Price * float64(v.Quantity)
+	}
+	err := o.repository.PutOrder(ctx, *or)
+	if err != nil {
+		return nil, err
+	}
+	return or, nil
 }
 
 func NewService(r Repository) Service {
